6a82be9b6bdd44c04fd2d8ea54711b10e0549d4c
Yuang Zhang <152662309+AZLonerd@users.noreply.github.com>
2024-10-20 00:05:44 -0400

    Add files via upload (#35)

---
+++ SOLID_GRASP/BaseTask.java
@@ -0,0 +1,3 @@
+interface BaseTask {
+    void execute();
+}
---
+++ SOLID_GRASP/NormalTeamMember.java
@@ -0,0 +1,13 @@
+public class NormalTeamMember extends TeamMember implements ManageProject {
+    private String basicResponsibility;
+    public NormalTeamMember(String name, String emailAddress, String role, String basicResponsibility) {
+        super(name, emailAddress, role);
+        this.basicResponsibility = basicResponsibility;
+    }
+
+
+    public String getBasicResponsibility() {
+        //return the basic responsibility of the team member
+        return null;
+    }
+}
