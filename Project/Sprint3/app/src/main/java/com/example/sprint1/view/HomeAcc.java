faf594fb0471e45551bc540882095d37223b64fc
Tianrui Qi <tianrui.qi@icloud.com>
2024-10-30 02:25:42 -0400

    Sprint3: Setup

---
+++ Project/Sprint3/app/src/main/java/com/example/sprint1/view/HomeAcc.java
@@ -0,0 +1,35 @@
+package com.example.sprint1.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sprint1.R;
+
+
+public class HomeAcc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_acc);
+
+        this.buttonNavigationBar();
+    }
+
+    private void buttonNavigationBar() {
+        findViewById(R.id.view_acc_button_log).setOnClickListener(
+                v -> startActivity(new Intent(HomeAcc.this, HomeLog.class))
+        );
+        findViewById(R.id.view_acc_button_des).setOnClickListener(
+                v -> startActivity(new Intent(HomeAcc.this, HomeDes.class))
+        );
+        findViewById(R.id.view_acc_button_din).setOnClickListener(
+                v -> startActivity(new Intent(HomeAcc.this, HomeDin.class))
+        );
+        findViewById(R.id.view_acc_button_tra).setOnClickListener(
+                v -> startActivity(new Intent(HomeAcc.this, HomeTra.class))
+        );
+    }
+}
---
+++ Project/Sprint3/app/src/main/java/com/example/sprint1/view/HomeDin.java
@@ -0,0 +1,35 @@
+package com.example.sprint1.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sprint1.R;
+
+
+public class HomeDin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_din);
+
+        this.buttonNavigationBar();
+    }
+
+    private void buttonNavigationBar() {
+        findViewById(R.id.view_din_button_log).setOnClickListener(
+                v -> startActivity(new Intent(HomeDin.this, HomeLog.class))
+        );
+        findViewById(R.id.view_din_button_des).setOnClickListener(
+                v -> startActivity(new Intent(HomeDin.this, HomeDes.class))
+        );
+        findViewById(R.id.view_din_button_acc).setOnClickListener(
+                v -> startActivity(new Intent(HomeDin.this, HomeAcc.class))
+        );
+        findViewById(R.id.view_din_button_tra).setOnClickListener(
+                v -> startActivity(new Intent(HomeDin.this, HomeTra.class))
+        );
+    }
+}
